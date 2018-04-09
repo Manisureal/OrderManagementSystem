be0b86415be0cd0969ac2ee9d17b41e4f805eb26
Mansoor Zaman <manisureal@outlook.com>
2018-04-09 12:11:02 +0100

    Updated pages controller views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'product/index'
-
-  get 'products/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :product, only: :index
+  resources :products, only: :index
 
 
 end

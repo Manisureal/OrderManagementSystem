c97b106fcbfe07d3ff399f12b819eb69b4824273
Mansoor Zaman <manisureal@outlook.com>
2018-04-10 15:26:32 +0100

    Devise Installed and Add to order setup

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,9 @@
 class OrdersController < ApplicationController
 
   def show
-    @order = Order.find(params[:id])
+    # total_price
+    @order_lines = current_order.order_lines
+    # @order = Order.find(params[:id])
+    # @order = Order.where(params[:id] => session[:cart_id]).first
   end
 end
